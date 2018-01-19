ec9baf83d85b887fdfb2fe4077d5ab6f3c9b4aa1
artem <arc7an@gmail.com>
2018-01-19 12:45:37 +0300

    New implementation of dynchars function.

--- calculate_sequence_phys_profile.R
+++ calculate_sequence_phys_profile.R
@@ -23,23 +23,18 @@ dynchars<-function(seq, interval, tss, strand=c('forward','reverse')) {
     stop("Sequence must be a character vector containing A, C, G, T letters only")
   # if((tss - interval < 1) || (tss + interval > length(seq)))
   #   stop("Considered interval exceeds the size of the sequence")
-  strand<- match.arg(strand)
+  strand <- match.arg(strand)
   seq <- toupper(seq)
-  seq<-switch(strand,
-              forward=seq[(tss - 150):(tss + 250 - 1)],
-              reverse=seq[(tss - 250):(tss + 150 - 1)])
-
-
+  seq <- switch(strand, 
+              forward=seq,
+              reverse=rev(seq))
+  
+  seq <- seq[(tss - 150):(tss + 250 - 1)]
+  
   a<-3.4*10^(-10)
-  I<-c(7.6, 4.8, 8.2, 4.1)*10^(-44)
+  #I<-c(7.6, 4.8, 8.2, 4.1)*10^(-44)
   K<-c(227, 155, 220, 149)*10^(-20)
   V<-c(2.09, 1.43, 3.12, 2.12)*10^(-20)
-
-  # split_seq <- split(seq(nchar(seq)),unlist(strsplit(seq,'')))
-  # countA <- length(split_seq[['A']])
-  # countT <- length(split_seq[['T']])
-  # countG <- length(split_seq[['G']])
-  # countC <- length(split_seq[['C']])
 
   csA<-cumsum(seq=='A')
   csT<-cumsum(seq=='T')
@@ -51,34 +46,21 @@ dynchars<-function(seq, interval, tss, strand=c('forward','reverse')) {
   countG = csG[interval:length(csG)]-c(0, csG[1:(length(csG)-interval)])
   countC = csC[interval:length(csC)]-c(0, csC[1:(length(csC)-interval)])
 
-  if (strand==1) {
-    M<-cbind(countA, countT, countG, countC)/interval
+  
+  M <- switch(strand,
+              forward = cbind(countA, countT, countG, countC)/interval,
+              reverse = cbind(countT, countA, countC, countG)/interval)
 
-    Is<-as.numeric(M%*%I)#! numeric conversion
-    Ks<-as.numeric(M%*%K)
-    Vs<-as.numeric(M%*%V)
+  #Is <- as.numeric(M%*%I)#! numeric conversion
+  Ks <- as.numeric(M%*%K)
+  Vs <- as.numeric(M%*%V)
 
-    E0<-(8*(Ks*Vs)^0.5)* 6E23 / 4184
-    d<-((Ks*a^2)/Vs)^(0.5)/a;
-    gc = M[,3] + M[,4]
+  E0 <- (8*(Ks*Vs)^0.5)* 6E23 / 4184
+  d <- ((Ks*a^2)/Vs)^(0.5)/a;
+  gc = M[,3] + M[,4]
 
-    dynchars_return<-list(E0=E0, d=d, gc=gc)
-    return(dynchars_return)
-  } else {
-    M_comp<-cbind(countT, countA, countC, countG)/interval
-    M_comp<-apply(t(M_comp),1,rev)
-
-    Is<-as.numeric(M_comp%*%I)#! numeric conversion
-    Ks<-as.numeric(M_comp%*%K)
-    Vs<-as.numeric(M_comp%*%V)
-
-    E0<- 8*(Ks*Vs)^0.5  * 6E23 / 4184;
-    d<-((Ks*a^2)/Vs)^(0.5)/a;
-    gc = M_comp[,3] + M_comp[,4]
-
-    dynchars_return<-list(E0=E0, d=d, gc=gc)
-    return(dynchars_return)
-  }
+  dynchars_return<-list(E0=E0, d=d, gc=gc)
+  return(dynchars_return)
 }
 
 #' Title
