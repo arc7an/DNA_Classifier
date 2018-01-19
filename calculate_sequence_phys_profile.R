52bed86bc485bde967e4acae1ef006290db14570
A Sorokin <lptolik@gmail.com>
2018-01-19 11:36:05 +0300

    roxygen

--- calculate_sequence_phys_profile.R
+++ calculate_sequence_phys_profile.R
@@ -2,37 +2,48 @@ library(ape)
 library(reldna)
 library(parallel)
 
-dynchars<-function(seq, interval, tss, strand=1) {
+#' Title
+#'
+#' @param seq full sequence (chromosome)
+#' @param interval requested interval
+#' @param tss
+#' @param strand DNA strand
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dynchars<-function(seq, interval, tss, strand=c(1,2)) {
   seq<-unlist(strsplit(seq, ''))
 
   if (missing(interval))
   stop("Need to specify interval")
-  
+
   if(!is.character(seq))
     stop("Sequence must be a character vector containing A, C, G, T letters only")
   # if((tss - interval < 1) || (tss + interval > length(seq)))
   #   stop("Considered interval exceeds the size of the sequence")
-  
+
   seq <- toupper(seq)
   if (strand == 1) {
-    seq <- seq[(tss - 150):(tss + 250 - 1)]  
+    seq <- seq[(tss - 150):(tss + 250 - 1)]
   }
   else {
     seq <- seq[(tss - 250):(tss + 150 - 1)]
   }
-  
-  
+
+
   a<-3.4*10^(-10)
   I<-c(7.6, 4.8, 8.2, 4.1)*10^(-44)
   K<-c(227, 155, 220, 149)*10^(-20)
   V<-c(2.09, 1.43, 3.12, 2.12)*10^(-20)
-  
+
   # split_seq <- split(seq(nchar(seq)),unlist(strsplit(seq,'')))
   # countA <- length(split_seq[['A']])
   # countT <- length(split_seq[['T']])
   # countG <- length(split_seq[['G']])
   # countC <- length(split_seq[['C']])
-  
+
   csA<-cumsum(seq=='A')
   csT<-cumsum(seq=='T')
   csG<-cumsum(seq=='G')
@@ -42,39 +53,51 @@ dynchars<-function(seq, interval, tss, strand=1) {
   countT = csT[interval:length(csT)]-c(0, csT[1:(length(csT)-interval)])
   countG = csG[interval:length(csG)]-c(0, csG[1:(length(csG)-interval)])
   countC = csC[interval:length(csC)]-c(0, csC[1:(length(csC)-interval)])
-  
+
   if (strand==1) {
     M<-cbind(countA, countT, countG, countC)/interval
-  
+
     Is<-as.numeric(M%*%I)#! numeric conversion
     Ks<-as.numeric(M%*%K)
     Vs<-as.numeric(M%*%V)
-    
+
     E0<-(8*(Ks*Vs)^0.5)* 6E23 / 4184
     d<-((Ks*a^2)/Vs)^(0.5)/a;
     gc = M[,3] + M[,4]
-    
+
     dynchars_return<-list(E0=E0, d=d, gc=gc)
     return(dynchars_return)
   }
-  
+
   else {
     M_comp<-cbind(countT, countA, countC, countG)/interval
-    M_comp<-apply(t(M_comp),1,rev)   
-      
+    M_comp<-apply(t(M_comp),1,rev)
+
     Is<-as.numeric(M_comp%*%I)#! numeric conversion
     Ks<-as.numeric(M_comp%*%K)
     Vs<-as.numeric(M_comp%*%V)
-    
+
     E0<- 8*(Ks*Vs)^0.5  * 6E23 / 4184;
     d<-((Ks*a^2)/Vs)^(0.5)/a;
     gc = M_comp[,3] + M_comp[,4]
-    
+
     dynchars_return<-list(E0=E0, d=d, gc=gc)
     return(dynchars_return)
   }
 }
 
+#' Title
+#'
+#' @param tss_position
+#' @param extended_string
+#' @param ep_interval
+#' @param zout
+#' @param strand
+#'
+#' @return
+#' @export
+#'
+#' @examples
 calculate_EP_on_interval <- function(tss_position, extended_string, ep_interval=c(250, 150), zout=-480:239, strand=1) {
   #lseqspline1D(substr(e.coli_U00096.2, exp_tsss[i]-250, exp_tsss[i]+150), bound=c(50, 350), ref=251 )
   p <- lseqspline1D(substr(extended_string, tss_position-ep_interval[1], tss_position+ep_interval[2]), bound=c(50, 350), ref=251)
@@ -86,6 +109,19 @@ calculate_EP_on_interval <- function(tss_position, extended_string, ep_interval=
   }
 }
 
+#' Title
+#'
+#' @param dnaSeq
+#' @param dynamic_interval
+#' @param tss_position
+#' @param ep_interval
+#' @param zout
+#' @param strand
+#'
+#' @return
+#' @export
+#'
+#' @examples
 calculate_profile <- function(dnaSeq, dynamic_interval, tss_position, ep_interval=c(267, 217), zout=-480:239, strand=1) {
   #ep_interval = c(163, 213) for reverse, for forward c(267, 217), zout - the same
   dynRes <- dynchars(dnaSeq, dynamic_interval, tss_position, strand)
